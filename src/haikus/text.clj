383a921f8f1d0943abe01b4fdc47b9b092fe782f
Jacob Harris <jharris@nytimes.com>
2015-03-19 21:28:54 -0400

    Some initial work on text splitting

--- src/haikus/text.clj
+++ src/haikus/text.clj
@@ -6,9 +6,9 @@
     [haikus.db :as db])
    (:use [slingshot.slingshot :only [throw+ try+]]))
 
-(def split-into-sentences (nlp/make-sentence-detector "resources/en-sent.bin"))
-(def tokenize-into-words (nlp/make-tokenizer "resources/en-token.bin"))
-
+;(def parser-file (io/file (io/resource "archieml.bnf" )))
+(def split-into-sentences (nlp/make-sentence-detector (io/file (io/resource "en-sent.bin"))))
+(def tokenize-into-words (nlp/make-tokenizer (io/file (io/resource "resources/en-token.bin"))))
 
 (def numbers-to-names
     {"0" "ZERO", "1" "ONE", "2" "TWO", "3" "THREE", "4" "FOUR", "5" "FIVE", "6" "SIX", "7" "SEVEN",
@@ -16,49 +16,41 @@
     "15" "FIFTEEN", "16" "SIXTEEN", "17" "SEVENTEEN", "18" "EIGHTEEN", "19" "NINETEEN", "20" "TWENTY",
     "30" "THIRTY", "40" "FORTY", "50" "FIFTY", "60" "SIXTY", "70" "SEVENTY", "80" "EIGHTY", "90" "NINETY"})
 
-; Taken from old clojure contrib
-(defmacro cond-let
-  "Takes a binding-form and a set of test/expr pairs. Evaluates each test
-  one at a time. If a test returns logical true, cond-let evaluates and
-  returns expr with binding-form bound to the value of test and doesn't
-  evaluate any of the other tests or exprs. To provide a default value
-  either provide a literal that evaluates to logical true and is
-  binding-compatible with binding-form, or use :else as the test and don't
-  refer to any parts of binding-form in the expr. (cond-let binding-form)
-  returns nil."
-  [bindings & clauses]
-  (let [binding (first bindings)]
-    (when-let [[test expr & more] clauses]
-      (if (= test :else)
-        expr
-        `(if-let [~binding ~test]
-           ~expr
-           (cond-let ~bindings ~@more))))))
-
 (defn normalized-term
   "Normalizes the term for checking in the DB"
   [term]
   (str/upper-case (str/replace term #"[^\w\d]$" "")))
 
+(defn find-term
+  "Looks up the term in the DB. Otherwise, it returns false"
+  [term]
+
+  (db/find-term (normalized-term term)))
+
 (defn find-term-or-throw
   "Looks up the term in the DB. Otherwise, it throws an exception"
   [term]
-  (let [normalized (normalized-term term)] 
-    (if-let [count (db/find-term normalized)]
-       count
-       (throw+ {:type :miss :term term :normalized normalized}))))
 
-(comment defn syllable-count
+  (if-let [count (find-term term)]
+    count
+    (throw+ {:type :miss :term term :normalized normalized})))
+
+(defn hyphenated-term?
+  "Returns a vector of the parts if it is, false otherwise"
+  [term]
+
+  (let [parts (str/split term #"\-")]
+    (if (> (count parts) 1)
+      (rest parts)
+      false)))
+
+
+(defn syllable-count
   "Returns the syllable count for a term or raises an exception"
   [term]
 
-  (cond-let [r (re-find #"^(\w+)\-(\w+)$" term)]
-              (+ (syllable-count (nth r 1))
-                 (syllable-coint (nth r 2)))
-            [r (re-find #"^(anti|un|non|pre|post|re|micro|super|hyper|mega|over|micro|cyber|ultra)(.+)" term)]
-              (+ (syllable-count (nth r 1))
-                 (syllable-count (nth r 2)))
-            :else
-              (find-term-or-throw term)))
-
-
+  (if-let [parts (hyphenated-term? term)]
+    (reduce + 0 (map syllable-count parts))
+    (if-let [])
+    )
+)
--- test/haikus/text_test.clj
+++ test/haikus/text_test.clj
@@ -13,3 +13,17 @@
 (with-redefs [haikus.db/find-term #(if (% == "ROBOT") 2 false)]
   (expect 2 (find-term-or-throw "robot"))
   (expect Exception (find-term-or-throw "cassowary")))
+
+(def test-lookup
+  [term]
+
+  (case term
+    "robot" 2
+    "self" 1
+    "inflicted" 3
+    :else false))
+
+(with-redefs [find-term-or-throw test-lookup]
+  (expect 2 (syllable-count "robot"))
+  (expect 4 (syllable-count "self-inflicted"))
+)
